b3c12b8a51402f590e15c35540db747c50bc0ccf
ghazal <ghazalkh.138@gmail.com>
2024-05-23 14:27:19 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 # TODO your deploy script implementation...
+pip install requirements.txt
+
+# Check if app.py is already running
+if pgrep -x "python3" > /dev/null; then
+    # Find the PID of the existing process
+    app_pid=$(pgrep -x "python3")
+    # Stop the existing process
+    kill "$app_pid"
+fi
+
+# Start app.py
+python3 app.py &
+
+
+
